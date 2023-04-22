b28a3a80ef1a05ad438976858919c49c2b335aad
Christiano112 <enyiachristian12@gmail.com>
2023-04-21 20:32:57 -0700

    fixed bug

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Executes a command that kills a process named killmenu
 
 exec { 'kill':
-    command => 'pkill -f killmenu',
+    command => 'pkill -f killmenow',
     path    => ['/usr/bin', '/usr/sbin']
 }
